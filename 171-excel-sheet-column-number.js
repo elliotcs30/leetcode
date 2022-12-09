2b07dc19622bfb395386e8a1280590cf557514b5
Elliot <elliotcs30@gmail.com>
2022-12-09 21:09:51 +0800

    feat: add #171 excel sheet column number soultion.

---
+++ 171-excel-sheet-column-number.js
@@ -0,0 +1,66 @@
+/**
+ * #171 Excel Sheet Column Number
+ */
+
+/**
+ * Related to question Excel Sheet Column Title.
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * 
+ * 
+ * For example:
+ * 
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * z -> 26
+ * AA -> 27
+ * AB -> 28
+ * 
+ * 
+ * 題目翻譯：
+ * 將Excel欄位轉換成數字。
+ */
+
+
+
+/**
+ * Example 1:
+ * 
+ * Input: columnTitle = "A"
+ * Output: 1
+ */
+
+/**
+ * Example 2:
+ * 
+ * Input: columnTitle = "AB"
+ * Output: 28
+ */
+
+/**
+ * Example 3:
+ * 
+ * Input: columnTitle = "ZY"
+ * Output: 701
+ */
+
+/**
+ * 參考思路：
+ * 1. A - Z 總共 26 個字母，由此可知這是 26 進位的系統
+ * 2. 將字串分別取出 A - Z，根據 ANSI CODE，A的code為65，A = 65 - 64 = 1
+ * 3. 以 AB 為例，AB = (A)26^1 + (B)26^0 = 126 + 2 * 1 = 28
+ * 4. 以 AZ 為例，AZ = (A)26^1 + (Z)26^0 = 126 + 26 * 1 = 52
+ */
+
+const titleToNumber = (s) => {
+  let sum = 0
+  let exp = 0
+  for (let i = s.length -1; i >= 0; i--) {
+    let v = s.charCodeAt(i) - 64
+    v = v * Math.pow(26, exp++)
+    sum += v
+  }
+  return sum
+}
