479cfb95fd00af57c8e0b7968fd4ad4a7960d252
Elliot <elliotcs30@gmail.com>
2023-01-18 22:41:41 +0800

    refactor: #88 merge sorted array

--- 88-merge-sorted-array.js
+++ 88-merge-sorted-array.js
@@ -3,28 +3,27 @@
  */
 
 /**
- * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
  * 
- * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
  * 
- * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ * 
  *
  * 題目翻譯：
  * 給兩個已經排序過的整數陣列 nums1 與 nums2，將 nums2 合併入 nums1 之中。
  * 
- * nums1會有有足夠的空間可以塞入兩個陣列(nums1.length = m + n)，
- * m 為 nums1 的元素數量，n 為 nums2 的元素數量。
- * 
- * 範例： 
- * nums1 = [1, 1, 2, 4, 6, null, null, null], m = 5, 
- * nums2 = [2, 3, 7], n = 3
- * 合併後 nums1 = [1, 1, 2, 2, 3, 4, 6, 7]
+ * nums1 和 nums2 中初始化的元素個數分別為 m 和 n。
+ * 假設 nums1 有足夠的空間（大小大於或等於 m + n ）來容納 nums2 中的其他元素。
  */
 
 /**
  * Example 1:
  * 
- * Input: nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
+ * Input: 
+ * nums1 = [1, 2, 3, 0, 0, 0], m = 3
+ * nums2 = [2, 5, 6],          n = 3
+ * 
  * Output: [1, 2, 2, 3, 5, 6]
  * 
  * Explanation: 
@@ -35,7 +34,10 @@
 /**
  * Example 2:
  * 
- * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Input: 
+ * nums1 = [1], m = 1, 
+ * nums2 = [], n = 0
+ * 
  * Output: [1]
  * 
  * Explanation: 
@@ -46,7 +48,10 @@
 /**
  * Example 3:
  * 
- * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Input: 
+ * nums1 = [0], m = 0, 
+ * nums2 = [1], n = 1
+ * 
  * Output: [1]
  * 
  * Explanation:
@@ -56,31 +61,11 @@
  * The 0 is only there to ensure the merge result can fit in nums1.
  */
 
-/**
- * 這題分成兩個步驟:
- * (1)先把 nums2 的值放到 nums1 裡面
-ex. nums1 = [1, 2, 6, null, null] , nums2 = [4, 5] --> [1, 2, 6, 4, 5]
- * 然後將nums1裡面的元素重新排序 [1, 2, 4, 5, 6]
- */
-
 const merge = function(nums1, m, nums2, n) {
-
-  let index = 0
-  
-  // 將nums2裡面的值放在nums1
-  for (let i = m; i < m + n; i++) {
-    nums1[i] = nums2[index]
-    index++
-  }
-
-  // 使用泡沫排序法重新排序
-  for (let j = 0; j < nums1.length - 1; j++) {
-    for (let k = j + 1; k < nums1.length; k++) {
-      if (nums1[j] > nums1[k]) {
-        let temp = nums1[j]
-        nums1[j] = nums1[k]
-        nums1[k] = temp
-      }
-    }
+  let i = m - 1
+  let j = n - 1
+  let k = m + n - 1
+  while (j >= 0) {
+    nums1[k--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--]
   }
 }
