2d5baf53b32e8edcc5e194c0036bb4d3370bb196
Elliot <elliotcs30@gmail.com>
2023-02-17 15:04:29 +0800

    modify: #349 format and unnecessary content

--- 349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays.js
@@ -12,25 +12,18 @@
 /**
  * Example 1:
  * 
- * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2]
  * Output: [2]
  */
 
 /**
  * Example 2:
  * 
- * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
- * Output: [9,4]
+ * Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4]
+ * Output: [9, 4]
  * 
  * Explanation: 
- * [4,9] is also accepted.
- */
-
-/**
- * Example 3:
- * 
- * Input: nums = [3, 3], target = 6
- * Output: [0, 1]
+ * [4, 9] is also accepted.
  */
 
 /**
@@ -38,8 +31,12 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2]
+
 const intersection = function(nums1, nums2) {
   let result = []
+
   for (let i = 0; i < nums1.length; i++) {
     for (let j = 0; j < nums2.length; j++) {
       if (nums1[i] === nums2[j]) result.push(nums1[i])
