299131251125b43ed6f321e5b6921b05e2be3974
Elliot <elliotcs30@gmail.com>
2023-05-18 23:33:58 +0800

    modify: add #1 explain and thking

--- Python/1-two-sum.py
+++ Python/1-two-sum.py
@@ -25,14 +25,26 @@ Example 3:
 
 Input: nums = [3,3], target = 6
 Output: [0,1]
+
+
+解釋：
+由題目可得知有一陣列為 nums，而期許的結果就是要把 nums 陣列中的數組兩兩相加相等於 target 的值，但相加的數組中不能使用相同的數字加總兩次，並且要回傳加總為 target 數字的位置。
 '''
+
+
+# 想法 1:
+# 把陣列中的數字兩兩相加，只要是等於 target 的值，那就回傳當前數字的位置。
+
+# pseudocode:
+# if nums[i] + nums[j] == target:
+#   return [i, j]
 
 nums = [3, 3]
 target = 6
 
 class Solution(object):
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
+                if nums[i] + nums[j] == target:
                     return [i, j]
