4b915a518aa39dfb7a1e1f1d6da339112908d1d0
Elliot <elliotcs30@gmail.com>
2023-05-21 23:46:07 +0800

    feat: update #561 explain and thiking

--- JavaScript/561-array-partition.js
+++ JavaScript/561-array-partition.js
@@ -33,7 +33,7 @@
  * min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9
  */
 
-/** ---解題思路---
+/** 想法 1:
  * step.1 將一陣列先做排序(sort)
  * step.2 再將陣列中的數值兩兩一組
  * step.3 在取得兩兩數組中最小的值
@@ -50,21 +50,42 @@
  * @return {number}
  */
 
- function compareNumbers(a, b) {
+  function compareNumbers(a, b) {
     return a - b;
   }
   
+  // var arrayPairSum = function(nums) {
+  //   nums.sort(compareNumbers)
+    
+  //   const newNums = []
+  //   let total = 0
+    
+  //   for (let i = 0; i < nums.length; i += 2) {
+  //     newNums.push(nums.slice(i, i+2)) 
+  //   }
+  //   for (let j = 0; j < newNums.length; j++) {
+  //     total += newNums[j][0]
+  //   }
+  //   return total
+  // };
+
+
+  /** 想法 2:
+   * 思考優化解:
+   * 試想是否可以用一次的 for 迴圈來達成，由題目可以觀察出將一陣列分為兩兩一組，再將最小的值相加。
+   * 因此可以推斷出就是把陣列中第 1、3、5、7... (i + 2) 相加後就是要返回的數字。
+   * 
+   * pseudocode:
+   * nums.sort()
+   * total = nums[0] + nums[2] + nums[4]...
+   */
+
   var arrayPairSum = function(nums) {
     nums.sort(compareNumbers)
-    
-    const newNums = []
     let total = 0
-    
+  
     for (let i = 0; i < nums.length; i += 2) {
-      newNums.push(nums.slice(i, i+2)) 
-    }
-    for (let j = 0; j < newNums.length; j++) {
-      total += newNums[j][0]
+      total += nums[i]
     }
     return total
   };
